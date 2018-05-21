d48e954ca16c8d1408fd68bc7fbdb77994728784
David Schmidt <dschmidt13@gmail.com>
2018-05-20 23:14:55 -0700

    Adding a default and recently used color palette to the gene editor.

--- merle-generator/src/org/jdawg/merle/ColorGeneEditorController.java
+++ merle-generator/src/org/jdawg/merle/ColorGeneEditorController.java
@@ -7,8 +7,13 @@ package org.jdawg.merle;
 
 import java.net.URL;
 import java.time.Instant;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Set;
 import java.util.TimerTask;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -23,7 +28,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
@@ -34,13 +41,23 @@ import javafx.scene.paint.Color;
  */
 public class ColorGeneEditorController implements Initializable
 {
+	// Class constants.
+	private static final String DEFAULT_PALETTE_URL_PATH = "/images/palette-default.png";
+
+	/**
+	 * Consistent with the number of squares on a default palette row.
+	 */
+	private static final int RECENT_PALETTE_MAX_SIZE = 11;
+
+	private static final int PALETTE_SQUARE_SIZE = 18;
+
 	// Private data members.
 	private ColorGene fieldGene;
 	private MerleService fieldPatternService;
 	private List<ColorGene> fieldServiceGenes;
-	private List<Color> fieldBasePaletteColors;
-	private List<Color> fieldRecentPaletteColors;
 	private TimerTask fieldLastPatternAutoRefreshTask;
+	private List<Color> fieldBasePaletteColors;
+	private Deque<Color> fieldRecentPaletteColors;
 
 	// Injected FXML members.
 	@FXML
@@ -117,12 +134,33 @@ public class ColorGeneEditorController implements Initializable
 	 */
 	public void actRefreshSamplePattern( )
 	{
+		// TODO - Calculate whether a dark or light background would be more appropriate
+		// for the current color, and set it into the service. How about: more than one
+		// value above 75%? Dark background. Otherwise, light background. Consider alpha
+		// somehow as well.
+
 		fieldServiceGenes.clear( );
 		fieldServiceGenes.add( paramsToColorGene( ) );
 
 		fieldPatternService.restart( );
 
 	} // actRefreshSamplePattern
+
+
+	private void addRecentPaletteColor( )
+	{
+		Color color = getColor( );
+		if ( !fieldBasePaletteColors.contains( color ) )
+			{
+			if ( fieldRecentPaletteColors.size( ) >= RECENT_PALETTE_MAX_SIZE )
+				fieldRecentPaletteColors.poll( );
+
+			fieldRecentPaletteColors.offer( getColor( ) );
+
+			redrawRecentPalette( );
+			}
+
+	} // addRecentPaletteColor
 
 
 	public void destroy( )
@@ -154,13 +192,22 @@ public class ColorGeneEditorController implements Initializable
 		// Someday we might want to do validation or something here.
 		fieldPatternService.cancel( );
 		acceptChanges( );
+		addRecentPaletteColor( );
 
 	} // handleOk
 
 
 	private void handlePaletteClick( MouseEvent event )
 	{
-		// TODO
+		if ( event.getSource( ) instanceof ImageView )
+			{
+			Image palette = ( ( ImageView ) event.getTarget( ) ).getImage( );
+			int x = ( int ) Math.round( event.getX( ) );
+			int y = ( int ) Math.round( event.getY( ) );
+
+			Color color = palette.getPixelReader( ).getColor( x, y );
+			setColor( color );
+			}
 
 	} // handlePaletteClick
 
@@ -204,7 +251,30 @@ public class ColorGeneEditorController implements Initializable
 
 	private void initPalettes( )
 	{
-		// TODO
+		// Collections for colors in the base palette and colors we've added to our own
+		// custom palette.
+		fieldBasePaletteColors = new ArrayList<>( );
+		fieldRecentPaletteColors = new ArrayDeque<>( RECENT_PALETTE_MAX_SIZE );
+
+		// Load the base palette image.
+		try
+			{
+			Image defaultPalette = new Image( DEFAULT_PALETTE_URL_PATH );
+			fieldPalette.setImage( defaultPalette );
+			fieldBasePaletteColors.addAll( scanPaletteColors( defaultPalette ) );
+			}
+		catch ( Exception exception )
+			{
+			// TODO Auto-generated catch block
+			exception.printStackTrace( );
+			}
+
+		// Draw a default recent palette.
+		redrawRecentPalette( );
+
+		// Add a click listener for both palettes.
+		fieldPalette.addEventFilter( MouseEvent.MOUSE_CLICKED, this::handlePaletteClick );
+		fieldRecentPalette.addEventFilter( MouseEvent.MOUSE_CLICKED, this::handlePaletteClick );
 
 	} // initPalettes
 
@@ -256,6 +326,60 @@ public class ColorGeneEditorController implements Initializable
 		gfx.fillRect( 0, 0, fieldSampleColor.getWidth( ), fieldSampleColor.getHeight( ) );
 
 	} // redrawSample
+
+
+	private void redrawRecentPalette( )
+	{
+		Canvas canvas = new Canvas( fieldRecentPalette.getFitWidth( ),
+				fieldRecentPalette.getFitHeight( ) );
+
+		GraphicsContext gfx = canvas.getGraphicsContext2D( );
+
+		gfx.setFill( Color.WHITE );
+		gfx.fillRect( 0, 0, canvas.getWidth( ), canvas.getHeight( ) );
+
+		int boxWidth = ( int ) canvas.getWidth( ) / RECENT_PALETTE_MAX_SIZE;
+		int xOffset = 0;
+		for ( Color color : fieldRecentPaletteColors )
+			{
+			gfx.setFill( color );
+			gfx.fillRect( xOffset, 0, boxWidth, canvas.getHeight( ) );
+			xOffset += boxWidth;
+			}
+
+		fieldRecentPalette.setImage( canvas.snapshot( null, null ) );
+
+	} // redrawRecentPalette
+
+
+	private Set<Color> scanPaletteColors( Image image )
+	{
+		Set<Color> colors = new HashSet<>( );
+
+		int width = ( int ) image.getWidth( );
+		int height = ( int ) image.getHeight( );
+		PixelReader reader = image.getPixelReader( );
+		for ( int yIndex = 0; yIndex < height; yIndex++ )
+			{
+			for ( int xIndex = 0; xIndex < width; xIndex++ )
+				{
+				colors.add( reader.getColor( xIndex, yIndex ) );
+				}
+			}
+
+		return colors;
+
+	} // scanBasePaletteColors
+
+
+	private void setColor( Color color )
+	{
+		fieldRed.setValue( color.getRed( ) * 255.0 );
+		fieldGreen.setValue( color.getGreen( ) * 255.0 );
+		fieldBlue.setValue( color.getBlue( ) * 255.0 );
+		fieldAlpha.setValue( color.getOpacity( ) * 255.0 );
+
+	} // setColor
 
 
 	public void setColorGene( ColorGene gene )
