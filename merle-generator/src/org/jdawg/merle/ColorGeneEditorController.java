ba6a2871fa8015b81f84060d4abbf791ad876026
David Schmidt <dschmidt13@gmail.com>
2018-06-12 00:56:53 -0700

    Refactoring ColorSelector out of ColorGeneEditor.

--- merle-generator/src/org/jdawg/merle/ColorGeneEditorController.java
+++ merle-generator/src/org/jdawg/merle/ColorGeneEditorController.java
@@ -7,16 +7,11 @@ package org.jdawg.merle;
 
 import java.net.URL;
 import java.time.Instant;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Deque;
-import java.util.HashSet;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.Set;
 import java.util.TimerTask;
 
+import org.jdawg.fxcomponent.ColorSelector;
 import org.jdawg.fxcontrol.NumericTextField;
 import org.jdawg.util.FXUtils;
 
@@ -27,13 +22,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.image.PixelReader;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 /**
@@ -43,59 +34,17 @@ import javafx.scene.paint.Color;
  */
 public class ColorGeneEditorController implements Initializable
 {
-	// Class constants.
-	private static final String DEFAULT_PALETTE_URL_PATH = "/images/palette-default.png";
-
-	/**
-	 * Consistent with the number of squares on a default palette row.
-	 */
-	private static final int RECENT_PALETTE_MAX_SIZE = 11;
-
-	private static final int PALETTE_SQUARE_SIZE = 18;
-
 	// Private data members.
 	private ColorGene fieldGene;
 	private GenerateCoatService fieldPatternService;
 	private TimerTask fieldLastPatternAutoRefreshTask;
-	private List<Color> fieldBasePaletteColors;
-	private Deque<Color> fieldRecentPaletteColors;
 
 	// Injected FXML members.
 	@FXML
 	private TextField fieldName;
 
 	@FXML
-	private ImageView fieldPalette;
-
-	@FXML
-	private ImageView fieldRecentPalette;
-
-	@FXML
-	private Slider fieldRed;
-
-	@FXML
-	private NumericTextField fieldRedText;
-
-	@FXML
-	private Slider fieldGreen;
-
-	@FXML
-	private NumericTextField fieldGreenText;
-
-	@FXML
-	private Slider fieldBlue;
-
-	@FXML
-	private NumericTextField fieldBlueText;
-
-	@FXML
-	private Slider fieldAlpha;
-
-	@FXML
-	private NumericTextField fieldAlphaText;
-
-	@FXML
-	private Canvas fieldSampleColor;
+	private ColorSelector fieldColorSelector;
 
 	@FXML
 	private Slider fieldConversionProbability;
@@ -144,34 +93,21 @@ public class ColorGeneEditorController implements Initializable
 	} // actRefreshSamplePattern
 
 
-	private void addRecentPaletteColor( )
-	{
-		Color color = getColor( );
-		if ( !fieldBasePaletteColors.contains( color )
-				&& !fieldRecentPaletteColors.contains( color ) )
-			{
-			if ( fieldRecentPaletteColors.size( ) >= RECENT_PALETTE_MAX_SIZE )
-				fieldRecentPaletteColors.poll( );
-
-			fieldRecentPaletteColors.offer( getColor( ) );
-
-			redrawRecentPalette( );
-			}
-
-	} // addRecentPaletteColor
-
-
 	private Color choosePatternBackground( )
 	{
 		final int LIGHT_THRESHOLD = 255 * 13 / 15;
 
+		Color color = fieldColorSelector.getColor( );
+
 		int lightCount = 0;
-		if ( fieldRed.getValue( ) > LIGHT_THRESHOLD )
+		if ( color.getRed( ) > LIGHT_THRESHOLD )
 			lightCount++;
-		if ( fieldGreen.getValue( ) > LIGHT_THRESHOLD )
+		if ( color.getGreen( ) > LIGHT_THRESHOLD )
 			lightCount++;
-		if ( fieldBlue.getValue( ) > LIGHT_THRESHOLD )
+		if ( color.getBlue( ) > LIGHT_THRESHOLD )
 			lightCount++;
+
+		// FIXME - Account for alpha.
 
 		return ( lightCount > 1 ? Color.BLACK : Color.WHITE );
 
@@ -183,16 +119,6 @@ public class ColorGeneEditorController implements Initializable
 		fieldPatternService.cancel( );
 
 	} // destroy
-
-
-	private Color getColor( )
-	{
-		Color color = new Color( fieldRed.getValue( ) / 255.0, fieldGreen.getValue( ) / 255.0,
-				fieldBlue.getValue( ) / 255.0, fieldAlpha.getValue( ) / 255.0 );
-
-		return color;
-
-	} // getColor
 
 
 	public void handleCancel( ActionEvent event )
@@ -207,41 +133,9 @@ public class ColorGeneEditorController implements Initializable
 		// Someday we might want to do validation or something here.
 		fieldPatternService.cancel( );
 		acceptChanges( );
-		addRecentPaletteColor( );
+		fieldColorSelector.select( );
 
 	} // handleOk
-
-
-	private void handlePaletteClick( MouseEvent event )
-	{
-		if ( event.getSource( ) instanceof ImageView )
-			{
-			Image palette = ( ( ImageView ) event.getTarget( ) ).getImage( );
-			int x = ( int ) Math.round( event.getX( ) );
-			int y = ( int ) Math.round( event.getY( ) );
-
-			Color color = palette.getPixelReader( ).getColor( x, y );
-			setColor( color );
-			}
-
-	} // handlePaletteClick
-
-
-	private void initColorSliders( )
-	{
-		// Configure sample redraw listeners.
-		fieldRed.valueProperty( ).addListener( ( obs, old, nw ) -> redrawColorSample( ) );
-		fieldGreen.valueProperty( ).addListener( ( obs, old, nw ) -> redrawColorSample( ) );
-		fieldBlue.valueProperty( ).addListener( ( obs, old, nw ) -> redrawColorSample( ) );
-		fieldAlpha.valueProperty( ).addListener( ( obs, old, nw ) -> redrawColorSample( ) );
-
-		// Configure slider/text field bindings.
-		fieldRedText.setNumberProperty( fieldRed.valueProperty( ), 0 );
-		fieldGreenText.setNumberProperty( fieldGreen.valueProperty( ), 0 );
-		fieldBlueText.setNumberProperty( fieldBlue.valueProperty( ), 0 );
-		fieldAlphaText.setNumberProperty( fieldAlpha.valueProperty( ), 0 );
-
-	} // jiggleInitSliders
 
 
 	@Override
@@ -249,49 +143,14 @@ public class ColorGeneEditorController implements Initializable
 	{
 		// Initialize sliders & listeners.
 		initService( );
-		initPalettes( );
-		initColorSliders( );
 		initPatternSliders( );
 
 		// Set the UI defaults.
 		setColorGene( null );
 
-		// Draw the first color swatch.
-		redrawColorSample( );
-
 		// NOTE: Don't refresh the pattern right away, as we have no color to draw.
 
 	} // initialize
-
-
-	private void initPalettes( )
-	{
-		// Collections for colors in the base palette and colors we've added to our own
-		// custom palette.
-		fieldBasePaletteColors = new ArrayList<>( );
-		fieldRecentPaletteColors = new ArrayDeque<>( RECENT_PALETTE_MAX_SIZE );
-
-		// Load the base palette image.
-		try
-			{
-			Image defaultPalette = new Image( DEFAULT_PALETTE_URL_PATH );
-			fieldPalette.setImage( defaultPalette );
-			fieldBasePaletteColors.addAll( scanPaletteColors( defaultPalette ) );
-			}
-		catch ( Exception exception )
-			{
-			// TODO Auto-generated catch block
-			exception.printStackTrace( );
-			}
-
-		// Draw a default recent palette.
-		redrawRecentPalette( );
-
-		// Add a click listener for both palettes.
-		fieldPalette.addEventFilter( MouseEvent.MOUSE_CLICKED, this::handlePaletteClick );
-		fieldRecentPalette.addEventFilter( MouseEvent.MOUSE_CLICKED, this::handlePaletteClick );
-
-	} // initPalettes
 
 
 	private void initPatternSliders( )
@@ -331,17 +190,6 @@ public class ColorGeneEditorController implements Initializable
 	} // paramsToColorGene
 
 
-	private void redrawColorSample( )
-	{
-		GraphicsContext gfx = fieldSampleColor.getGraphicsContext2D( );
-		gfx.setFill( Color.WHITE );
-		gfx.fillRect( 0, 0, fieldSampleColor.getWidth( ), fieldSampleColor.getHeight( ) );
-		gfx.setFill( getColor( ) );
-		gfx.fillRect( 0, 0, fieldSampleColor.getWidth( ), fieldSampleColor.getHeight( ) );
-
-	} // redrawSample
-
-
 	private void redrawPattern( WorkerStateEvent event )
 	{
 		if ( event != null && event.getSource( ).getState( ) == State.SUCCEEDED )
@@ -361,65 +209,11 @@ public class ColorGeneEditorController implements Initializable
 	} // redrawPattern
 
 
-	private void redrawRecentPalette( )
-	{
-		Canvas canvas = new Canvas( fieldRecentPalette.getFitWidth( ),
-				fieldRecentPalette.getFitHeight( ) );
-
-		GraphicsContext gfx = canvas.getGraphicsContext2D( );
-
-		gfx.setFill( Color.WHITE );
-		gfx.fillRect( 0, 0, canvas.getWidth( ), canvas.getHeight( ) );
-
-		int boxWidth = ( int ) canvas.getWidth( ) / RECENT_PALETTE_MAX_SIZE;
-		int xOffset = 0;
-		for ( Color color : fieldRecentPaletteColors )
-			{
-			gfx.setFill( color );
-			gfx.fillRect( xOffset, 0, boxWidth, canvas.getHeight( ) );
-			xOffset += boxWidth;
-			}
-
-		fieldRecentPalette.setImage( canvas.snapshot( null, null ) );
-
-	} // redrawRecentPalette
-
-
-	private Set<Color> scanPaletteColors( Image image )
-	{
-		Set<Color> colors = new HashSet<>( );
-
-		int width = ( int ) image.getWidth( );
-		int height = ( int ) image.getHeight( );
-		PixelReader reader = image.getPixelReader( );
-		for ( int yIndex = 0; yIndex < height; yIndex++ )
-			{
-			for ( int xIndex = 0; xIndex < width; xIndex++ )
-				{
-				colors.add( reader.getColor( xIndex, yIndex ) );
-				}
-			}
-
-		return colors;
-
-	} // scanBasePaletteColors
-
-
 	public void setAlgorithm( String algorithm )
 	{
 		fieldPatternService.setAlgorithm( algorithm );
 
 	} // setAlgorithm
-
-
-	private void setColor( Color color )
-	{
-		fieldRed.setValue( color.getRed( ) * 255.0 );
-		fieldGreen.setValue( color.getGreen( ) * 255.0 );
-		fieldBlue.setValue( color.getBlue( ) * 255.0 );
-		fieldAlpha.setValue( color.getOpacity( ) * 255.0 );
-
-	} // setColor
 
 
 	public void setColorGene( ColorGene gene )
@@ -429,10 +223,7 @@ public class ColorGeneEditorController implements Initializable
 		if ( gene == null )
 			{
 			fieldName.setText( "" );
-			fieldRed.setValue( 0 );
-			fieldGreen.setValue( 0 );
-			fieldBlue.setValue( 0 );
-			fieldAlpha.setValue( 0 );
+			fieldColorSelector.setColor( null );
 			fieldConversionProbability.setValue( 0 );
 			fieldCoolingRate.setValue( 0 );
 			fieldSignalStrength.setValue( 0 );
@@ -440,10 +231,7 @@ public class ColorGeneEditorController implements Initializable
 		else
 			{
 			fieldName.setText( gene.getName( ) );
-			fieldRed.setValue( Math.round( gene.getColor( ).getRed( ) * 255.0 ) );
-			fieldGreen.setValue( Math.round( gene.getColor( ).getGreen( ) * 255.0 ) );
-			fieldBlue.setValue( Math.round( gene.getColor( ).getBlue( ) * 255.0 ) );
-			fieldAlpha.setValue( Math.round( gene.getColor( ).getOpacity( ) * 255.0 ) );
+			fieldColorSelector.setColor( gene.getColor( ) );
 			fieldConversionProbability.setValue( gene.getSeedConversionProb( ) );
 			fieldCoolingRate.setValue( gene.getCoolingRate( ) );
 			fieldSignalStrength.setValue( gene.getSignalStrength( ) );
@@ -457,7 +245,7 @@ public class ColorGeneEditorController implements Initializable
 		if ( gene != null )
 			{
 			gene.setName( fieldName.getText( ) );
-			gene.setColor( getColor( ) );
+			gene.setColor( fieldColorSelector.getColor( ) );
 			gene.setSeedConversionProb( fieldConversionProbability.getValue( ) );
 			gene.setSignalStrength( fieldSignalStrength.getValue( ) );
 			gene.setCoolingRate( fieldCoolingRate.getValue( ) );
